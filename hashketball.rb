49998a0a023821c2a4c805b8491c71ec237a0a55
Dana Bliss <danajackson2@gmail.com>
2020-10-17 15:38:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,2 +1,40 @@
 def game_hash
-  
+  game_hash = {
+    :home =>{
+      :team_name => ""
+      :colors => ["",""]
+      :players =>[
+        {:player_name => "",
+        :number => 0,
+        :shoe => 14,
+        :points => 10,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0},
+        {},
+        {},
+        {}
+      ]
+    },
+    :away => {
+      :team_name => ""
+      :colors => ["",""]
+      :players =>[
+        {:player_name => "",
+        :number => 0,
+        :shoe => 14,
+        :points => 10,
+        :rebounds => 0,
+        :assists => 0,
+        :steals => 0,
+        :blocks => 0,
+        :slam_dunks => 0},
+        {},
+        {},
+        {}
+      ]
+    }
+  }
+end
